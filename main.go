e4dc22345a753e85b670838198b76b70474b2ec0
assanoff <assanoff@gmail.com>
2019-08-01 17:35:24 +0600

    first working draft

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/jpeg"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/nfnt/resize"
 )
 
 type Message struct {
@@ -50,5 +56,24 @@ func ImageHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("content-type", "application/json")
 	w.Write(output)
+
+	sourceImage, err := base64.StdEncoding.DecodeString(msg.Body)
+	fmt.Println("body:.............................: " + msg.Body)
+
+	img1, _, err := image.Decode(bytes.NewReader(sourceImage))
+
+	if err != nil {
+		fmt.Printf("Error decoding string: %s ", err.Error())
+		return
+	}
+
+	m := resize.Resize(200, 200, img1, resize.Lanczos3)
+	buf := new(bytes.Buffer)
+	err = jpeg.Encode(buf, m, nil)
+	imageBit := buf.Bytes()
+	/*Defining the new image size*/
+
+	photoBase64 := base64.StdEncoding.EncodeToString([]byte(imageBit))
+	fmt.Println("Photo Base64.............................: " + photoBase64)
 
 }
